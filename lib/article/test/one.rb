f5db4365afa62101bd0802de5d7f96d2927dca06
Gonçalo Morais <gnclmorais@gmail.com>
2020-10-31 17:49:55 +0000

    Add tests and implementation

--- lib/article/test/one.rb
+++ lib/article/test/one.rb
@@ -1,10 +1,2 @@
 require "article/test/one/version"
-
-module Article
-  module Test
-    module One
-      class Error < StandardError; end
-      # Your code goes here...
-    end
-  end
-end
+require "article/test/one/string"
